0c2bcdb7415b5dfa91f0dd5bf5b263cbda604329
renatoReboucas <renato.silva@triforsys.com.br>
2023-12-05 13:30:28 -0300

    wip

--- src/controller/company/company.controller.ts
+++ src/controller/company/company.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CompanyModel } from 'src/models/company.model';
 import { CompanySchema } from 'src/schemas/company.schemas';
@@ -17,8 +17,22 @@ export class CompanyController {
     return { data: company };
   }
   @Get()
-  public async index(): Promise<{ data: CompanyModel[] }> {
+  public async findAll(): Promise<{ data: CompanyModel[] }> {
     const list = await this.model.find();
     return { data: list };
+  }
+  @Get()
+  public async findOne() {
+    return `This action returns a  company`;
+  }
+
+  @Put()
+  public async update() {
+    return `This action updates a  company`;
+  }
+
+  @Delete()
+  public async remove() {
+    return `This action removes a  company`;
   }
 }
